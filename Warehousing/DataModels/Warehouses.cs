a559b175f0b81641b749cc9c5d8fb0f98ff6f012
makayru <mmsb03@outlook.com>
2024-10-23 20:30:16 +0200

    Made the warehouses model, Controller, Service, Json and Rest file

--- Warehousing/DataModels/Warehouses.cs
+++ Warehousing/DataModels/Warehouses.cs
@@ -1,23 +1,21 @@
-using System;
-using System.Collections.Generic;
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
 
-namespace Models
-{
     public class Warehouse
     {
         public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        public string Zip { get; set; }
         public string City { get; set; }
-        public string ZipCode { get; set; }
         public string Province { get; set; }
         public string Country { get; set; }
-        public string ContactName { get; set; }
-        public string ContactPhone { get; set; }
-        public string ContactEmail { get; set; }
+        public Contact Contact { get; set; }  // Nested object
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
-
-}
--- Warehousing/Program.cs
+++ Warehousing/Program.cs
@@ -6,14 +6,13 @@ using Microsoft.Extensions.Hosting;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<WarehouseService>();
+
 
 var app = builder.Build();
 
+app.Urls.Add("http://localhost:5000");
 
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthorization();
 app.MapControllers();
 
 
