021839d590398bb94391cb4fca993e1a91f612a7
makayru <mmsb03@outlook.com>
2024-10-23 15:04:11 +0200

    test

--- Warehousing/Program.cs
+++ Warehousing/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
 
 app.Run();
